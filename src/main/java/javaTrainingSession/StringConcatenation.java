422b33e37777f3c56e2c52c6406bf05449348ca1
debashis <debashis.padhi60@gmail.com>
2022-05-05 21:02:46 +0530

    string concatenation

---
+++ src/main/java/javaTrainingSession/StringConcatenation.java
@@ -0,0 +1,36 @@
+package javaTrainingSession;
+
+public class StringConcatenation {
+//
+	public static void main(String[] args) {
+int a = 100;
+int b = 500;
+String x = "hello";
+String y = " world";
+double c = 12.35;
+double d = 10.35;
+System.out.println(a+b);
+System.out.println(x+y);
+System.out.println(a+b+x+y);
+System.out.println(x+y+a+b);
+System.out.println(x+y+(a+b));
+System.out.println(a+b+x+y+a+x+b+y);
+System.out.println(a+b+x+y+a+b);
+System.out.println(c+d);
+System.out.println(x+y+c+d);
+System.out.println(c+d+x+y);
+System.out.println(a+b+c+d);
+System.out.println(a+c+b+d);
+System.out.println(x+c+y+d);
+System.out.println("helloworld");
+System.out.println(a);
+System.out.println(b);
+System.out.println("the value of a is;"+a);
+System.out.println("the value of b is;"+b);
+System.out.println("the value of a and b is;"+(a+b));
+
+
+
+	}
+
+}
